94ed9b6622ee9936c093fcffb3f101ca430288a6
almarrim <mosalmarri@gmail.com>
2019-11-07 10:00:16 +0300

    add comments to the app page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /*
 Recipe (App): Recipes is an app that generates cooking recipes based on a food item searched by the user. Recipes can be added to a list, marked as new/old and notes can be added to recipe cards in the list. You can visit the app here =>(https://almarrim.github.io/recipes/)
 for how to use it checkout the instructions in the README.md file.
+*****Disclaimer: This version of the code has parts of a merged branch that are not used here anymore. Due to the limited to submit this project and the errors created by removing parts of the unused codes(all related unused codes need to be removed), it was left here for the time being. They will be removed in future updates. Also, more comments will be added.*****
 Created by: Mohammed Almarri
 */
 
@@ -25,12 +26,12 @@ class App extends Component {
       noteBox: '',//used with notes to carry the text to the noteobject
       oldOnes: [],//this is the array that carries the old(tried) recipes
       searchBox: '',//this is the search box text in the search page
-      starsObject:{},
+      starsObject: {},
     };
   }
-/*
-getResults is where the axios gets called. getResults takes the searchBox text as item and pass it to the axios through the url. getResults then assign the results to recipes, where it the data get used to show the needed info. Also, refreshes the searchBox and set it empty again.
-*/
+  /*
+  getResults is where the axios gets called. getResults takes the searchBox text as item and pass it to the axios through the url. getResults then assign the results to recipes, where it the data get used to show the needed info. Also, refreshes the searchBox and set it empty again.
+  */
   getResults = (item) => {
     const apiURL = `http://api.edamam.com/search?q=${item}&app_id=${process.env.REACT_APP_EDMAME_API_ID}&ap_key=${process.env.REACT_APP_EDMAME_API_KE}&from=0&to=20`
     axios({
@@ -71,9 +72,9 @@ faveToggle is the function that is responsible for adding removing recipes from 
       favorites: []
     })
   }
-/*
-getText is the method responsible for taking the note text and passing it to the object with the related key, and it refreshes the textbox of the note tap. label is the recipe name(the key). noteBox is the value. e is the event.
-*/
+  /*
+  getText is the method responsible for taking the note text and passing it to the object with the related key, and it refreshes the textbox of the note tap. label is the recipe name(the key). noteBox is the value. e is the event.
+  */
   getText = (e, label, noteBox) => {
     e.preventDefault()
     const noteObject = this.state.noteObject;
@@ -88,11 +89,11 @@ getText is the method responsible for taking the note text and passing it to the
   //delete only the attached note
   deleteIt = (item) => {
     console.log('deleteIt')
-    const noteObject={...this.state.noteObject}
+    const noteObject = { ...this.state.noteObject }
     delete noteObject[item]
     this.setState({
       noteValue: '',
-      noteObject:noteObject
+      noteObject: noteObject
 
     })
   };
@@ -105,7 +106,7 @@ getText is the method responsible for taking the note text and passing it to the
     // this.setState({ noteBox: e.target.value })
 
   }
-  //
+  //not used for the current version. As well as all the following related methods related to stars
   resetNew = () => {
     this.setState({
       starsObject: {},
@@ -161,7 +162,9 @@ getText is the method responsible for taking the note text and passing it to the
     })
     this.props.setStars(index, label)
   }
-
+  /*
+  toggleMark toggles the status of the recipe card in the favorite page. It updates the oldOnes array which holds the data of cards set to Old status.
+  */
   toggleMark = (e) => {
     const oldOnes = this.state.oldOnes
     console.log('recipe is ', e, ' oldOnes ', oldOnes)
@@ -177,12 +180,16 @@ getText is the method responsible for taking the note text and passing it to the
       oldOnes: oldOnes
     });
   }
+  /*
+resetNew resets all recipe cards to New status.
+  */
   resetNew = () => {
     console.log('in resetnew')
     this.setState({ status: 'New', oldOnes: [] })
   }
-  resetNotes = ()=>{
-    this.setState({noteObject:{}})
+  //resetNote removes all the notes from the recipe cards
+  resetNotes = () => {
+    this.setState({ noteObject: {} })
   }
   render() {
     return (
@@ -193,7 +200,7 @@ getText is the method responsible for taking the note text and passing it to the
           <Link className="nav-item nav-link" to='/'>Search</Link>
           <Link className="nav-item nav-link" to='/mylist'>My List{' | '}{this.state.favorites.length}</Link>
         </nav>
-
+        {/* SearchPage and FavoritePage are the only direct children of the main App.js. the SearchPage is where the search happens and the search results are shown. The FavoritePage is where the list of the selected recipes are created. */}
         <Switch>
           <Route exact path='/' render={() => <SearchPage getResults={this.getResults} recipes={this.state.recipes} faveToggle={this.faveToggle} favorites={this.state.favorites} searchBox={this.state.searchBox} />} />
           <Route path='/mylist' render={() => <FavoritePage resetNotes={this.resetNotes} noteObject={this.state.noteObject} favorites={this.state.favorites} faveToggle={this.faveToggle} deleteAll={this.deleteAll} toggleMark={this.toggleMark} resetNew={this.resetNew} oldOnes={this.state.oldOnes} getText={this.getText} deleteIt={this.deleteIt} handleChange={this.handleChange} noteValue={this.state.noteValue} noteBox={this.state.noteBox} setStars={this.setStars} starsObject={this.state.starsObject} allStars={this.state.allStars} resetThis={this.resetThis} makeStars={this.makeStars} addStars={this.addStars} />} />
